4e77cb30d129ccd9a7f55764b3480517caf95e4d
ALUNO <aluno@ladmac0112.local>
2019-04-15 19:31:42 -0300

    Initial Commit

---
+++ ExemploWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExemploWatch
+//
+//  Created by ALUNO on 15/04/19.
+//  Copyright © 2019 Willian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
